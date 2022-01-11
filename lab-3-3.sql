87f4b7a036e7f044dfb1021370194d21f986b86c
premaltailor <premal.tailor@kellogg.northwestern.edu>
2022-01-11 17:05:06 +0000

    Finish lab 3 and 4-1

--- lab-3-3.sql
+++ lab-3-3.sql
@@ -15,9 +15,9 @@
 -- | 2019 | Houston Astros                | 107       |
 -- | 2004 | St. Louis Cardinals           | 105       |
 
-SELECT year, name, max(wins)
+SELECT year, name, MAX(wins)
 FROM teams
 WHERE year >= 1960
 GROUP BY year
-ORDER by wins desc;
+ORDER by MAX(wins) desc;
 
--- lab-4-1.sql
+++ lab-4-1.sql
@@ -3,4 +3,11 @@
 -- Expected result:
 -- 2935
 
+-- To Look up tables
+-- SELECT * FROM players WHERE first_name = "Barry" and last_name = "Bonds"
+-- SELECT * FROM stats WHERE player_ID = 1678;
 
+SELECT SUM(stats.hits)
+FROM players INNER JOIN stats ON players.id = stats.player_id
+WHERE players.first_name = "Barry" 
+AND  players.last_name = "Bonds";
